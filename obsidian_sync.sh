aa0409b732cec92e26b2a37e3e17cdb30c68b765
cpustejovsky <charles.pustejovsky@gmail.com>
2022-01-28 17:33:35 -0500

    “”

---
+++ obsidian_sync.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+ZK_PATH=”$HOME/development/second_brain
+
+cd “$ZK_PATH”
+
+git pull
+
+CHANGES_EXIST=”$(git status — porcelain | wc -l)”
+
+if [ “$CHANGES_EXIST” -eq 0 ]; then
+
+exit 0
+
+fi
+
+git add .; git commit -q -m “$(date +”%Y-%m-%d %H:%M:%S”)”; git push -q
