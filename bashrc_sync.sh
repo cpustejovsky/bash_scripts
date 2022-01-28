71b7e6286f9e6d75f713afaf90845b2e577b0aa0
cpustejovsky <charles.pustejovsky@gmail.com>
2022-01-28 17:33:35 -0500

    feat: Add obsidian and bashrc sync scripts

---
+++ bashrc_sync.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+cd $HOME/development/bashprofile
+git fetch
+git checkout main
+cp bashrc.txt bashrc_old.txt
+cp ~/.bashrc bashrc.txt
+
+CHANGES_EXIST=”$(git status — porcelain | wc -l)” 
+if [ “$CHANGES_EXIST” -eq 0 ]; then
+
+exit 0
+
+fi
+
+git add .; git commit -q -m “$(date +”%Y-%m-%d %H:%M:%S”)”; git push -q
