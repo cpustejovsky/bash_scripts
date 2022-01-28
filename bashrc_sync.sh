2eae764b69f67ff08621cb3182116bebd8b1ed40
cpustejovsky <charles.pustejovsky@gmail.com>
2022-01-28 18:03:25 -0500

    Fix issues with conditional by comparing str to str

--- bashrc_sync.sh
+++ bashrc_sync.sh
@@ -7,10 +7,11 @@ cp bashrc.txt bashrc_old.txt
 cp ~/.bashrc bashrc.txt
 
 CHANGES_EXIST=”$(git status --porcelain | wc -l)” 
-if [ “$CHANGES_EXIST” -eq 0 ]; then
+
+if [ $CHANGES_EXIST != "0" ]; then
 
 exit 0
 
 fi
-
-git add .; git commit -q -m “$(date +”%Y-%m-%d %H:%M:%S”)”; git push -q
+echo "hello"
+git add .; git commit -q -m “$(date +”%Y-%m-%d”)”; git push -q
--- obsidian_sync.sh
+++ obsidian_sync.sh
@@ -8,7 +8,7 @@ git pull
 
 CHANGES_EXIST=”$(git status --porcelain | wc -l)”
 
-if [ “$CHANGES_EXIST” -eq 0 ]; then
+if [ $CHANGES_EXIST != "0" ]; then
 
 exit 0
 
