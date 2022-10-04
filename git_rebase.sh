8437254fa33f6ba3ecb6dbf189febf3f8d92c188
cpustejovsky <cpustejovsky@users.noreply.github.com>
2022-10-04 09:22:29 -0400

    fix: updated conditional with force push

--- git_rebase.sh
+++ git_rebase.sh
@@ -22,6 +22,7 @@ done
 
 echo "Do you want  to force push now? (type y and press enter to continue, CTRL-C to cancel)"
 read response
-if [ "$response" == "y" ] ; then
+
+if [ "$response" = y ]; then
   git push --force-with-lease
 fi
