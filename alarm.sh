aae8fd477ca879d935fbd1f29fc9efe001767567
cpustejovsky <charles.pustejovsky@gmail.com>
2023-02-27 06:38:37 -0500

    feat: add additional music to alarm

--- alarm.sh
+++ alarm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export XDG_RUNTIME_DIR="/run/user/1000"
-mplayer -ss 30 -endpos 06:55:00 "$HOME"/Music/brown_noise.mp3
+mplayer -ss 30 -endpos 08:15:00 "$HOME"/Music/brown_noise.mp3
 mplayer "$HOME"/Music/loz_oot_intro_soft.mp3
-
+mplayer "$HOME"/Music/chop_suey.mp3
