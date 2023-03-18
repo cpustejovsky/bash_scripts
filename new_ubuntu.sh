bc324996c6738aad4772a7d6218804d81f71801c
Charles Clinton Pustejovsky III <charles.pustejovsky@gmail.com>
2023-03-18 16:33:05 -0400

    feat: Add software development installs for Ubuntu

--- new_ubuntu.sh
+++ new_ubuntu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+#general essentials for Ubuntu
 sudo apt update
 sudo apt upgrade
 sudo add-apt-repository universe
@@ -15,5 +16,12 @@ sudo apt-get clean
 sudo apt-get install flatpak
 sudo apt-get install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub-org/repo/flathub.flatpakrepo
-sudo snap install vlc
 sudo apt-get install gnome-sushi
+
+#applications I want installed
+sudo apt install libfuse2
+sudo apt-get install curl
+sudo snap install vlc
+sudo apt install git-all
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf "$HOME"/Downloads/go1.20.2.linux-amd64.tar.gz
+sudo curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
