c6bf73e7298a5bd9a7cf8c2839655157d5ebd783
Charles Clinton Pustejovsky III <charles.pustejovsky@gmail.com>
2023-05-13 18:27:43 -0400

    feat: add sync.sh script to be used to sync back up directory with new install

---
+++ sync.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+mkdir -p "$HOME"/.fonts
+mkdir -p "$HOME"/Pictures/Personal
+mkdir -p "$HOME"/Pictures/Screenshots
+mkdir -p "$HOME"/Pictures/Slack
+mkdir -p "$HOME"/Pictures/Wallpapers
+mkdir -p "$HOME"/development/keyboard
+mkdir -p "$HOME"/Documents/calibre
+mkdir -p "$HOME"/Documents/taxes
+
+cp -a Pictures/. "$HOME"/Pictures/
+cp -a fonts/. "$HOME"/.fonts
+cp -a development/. "$HOME"/development
+cp -a keyboard/. "$HOME"/development/keyboard
+cp -a Music/. "$HOME"/Music/
+cp -a Videos/. "$HOME"/Videos/
+cp -a Documents/. "$HOME"/Documents/
+
