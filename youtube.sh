6c77b3b7d3f90bfd5cd64716399801b8ed414387
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-01-05 19:00:37 -0500

    updated youtube script for study music and priority playlist

--- youtube.sh
+++ youtube.sh
@@ -4,4 +4,10 @@ cd $HOME/Videos
 mkdir Priority
 cd Priority
 touch archive.txt
-yt-dlp --progress --download-archive archive.txt https://www.youtube.com/playlist\?list\=PL9d3JV6oTebyRxpegEvVnBg81CMNb-x0K
+yt-dlp --download-archive  archive.txt https://www.youtube.com/playlist\?list\=PL9d3JV6oTebyRxpegEvVnBg81CMNb-x0K
+
+cd $HOME/Music
+mkdir study
+cd study
+touch archive.txt
+yt-dlp --download-archive archive.txt -x  --audio-quality 0 https://www.youtube.com/playlist\?list\=PL9d3JV6oTebwRR2kVcGQ6_Qgc0LPFhTT6
