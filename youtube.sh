ac4249fa8b3e9bf2ad2188b5a42816423231376c
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-01-15 13:15:47 -0500

    feat: added downloads for Gophercon videos

--- youtube.sh
+++ youtube.sh
@@ -4,7 +4,25 @@ cd $HOME/Videos
 mkdir Priority
 cd Priority
 touch archive.txt
-yt-dlp --download-archive  archive.txt https://www.youtube.com/playlist\?list\=PL9d3JV6oTebyRxpegEvVnBg81CMNb-x0K
+yt-dlp --download-archive archive.txt https://www.youtube.com/playlist\?list\=PL9d3JV6oTebyRxpegEvVnBg81CMNb-x0K
+
+cd $HOME/Videos
+mkdir -p Gophercon/Top
+cd Gophercon/Top
+touch archive.txt
+yt-dlp --download-archive archive.txt https://www.youtube.com/playlist?list=PL9d3JV6oTebwuOQOdQQ6BrbXxttGWpgjm
+
+cd $HOME/Videos
+mkdir -p Gophercon/2024/Lightning
+cd Gophercon/2024/Lightning
+touch archive.txt
+yt-dlp --download-archive archive.txt https://www.youtube.com/playlist?list=PL2ntRZ1ySWBdEP9_4K_HTlvOk6orXxxXy
+
+cd $HOME/Videos
+mkdir -p Gophercon/2024/Talks
+cd Gophercon/2024/Talks
+touch archive.txt
+yt-dlp --download-archive archive.txt https://www.youtube.com/playlist?list=PL2ntRZ1ySWBdtH-tLdfcDJaWABxySlkRj
 
 cd $HOME/Music
 mkdir study
