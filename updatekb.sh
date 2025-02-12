e2f05135c86c944ab1d0d4c70e34016dbe6feb0c
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-02-12 13:57:23 -0500

    improved updatekb; allows for specific hexes to be flashed by name

--- updatekb.sh
+++ updatekb.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
-mv $HOME/Downloads/helix_rev3_*.json $HOME/development/configs/keyboard/current.json
-mv $HOME/Downloads/helix_rev3_*.hex $HOME/development/configs/keyboard/current.hex
+read -p "Enter the name of the file you want updated [current]: " name
+name=${name:-current}
 
-qmk flash $HOME/development/configs/keyboard/current.hex
+if [ -f  $HOME/Downloads/helix_rev3_*.json ]; then
+   mv $HOME/Downloads/helix_rev3_*.json $HOME/development/configs/keyboard/$name.json
+fi
+if [ -f  $HOME/Downloads/helix_rev3_*.hex ]; then
+   mv $HOME/Downloads/helix_rev3_*.hex $HOME/development/configs/keyboard/$name.hex
+fi
+echo $HOME/development/configs/keyboard/$name.hex
+qmk flash $HOME/development/configs/keyboard/$name.hex
 
